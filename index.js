127992ca08a742b8acc402a4ec0b7bfd0bda763c
Aditya <jackuserme8881@gmail.com>
2024-11-11 20:15:34 +0530

    Platform and Rectangular collision Detection
    Added a platform.
    Coded rectangular collision detection to detect whether the player is on the platform or not.
    If the player is on the platform, it stays on it otherwise it falls right back

--- index.js
+++ index.js
@@ -38,7 +38,25 @@ class Player{
     }
 }
 
+class Platform{
+    constructor() {
+        this.position = {
+            x: 200,
+            y: 100
+        }
+        this.width = 200
+        this.height = 20        
+    }
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x,
+            this.position.y, this.width, this.height)
+        
+    }
+}
+
 const player = new Player()
+const platform = new Platform()
 const keys = {
     right: {
         pressed: false
@@ -53,13 +71,23 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+    platform.draw()
 
     if (keys.right.pressed) {
         player.velocity.x = 5
     }
     else if (keys.left.pressed) {
         player.velocity.x = -5
-   } else player.velocity.x = 0
+    } else player.velocity.x = 0
+    
+    if (player.position.y + player.height
+        <= platform.position.y &&
+        player.position.y + player.height + 
+        player.velocity.y >= platform.position.y &&
+        player.position.x + player.width >= platform.position.x &&
+player.position.x <= platform.position.x + platform.width) {
+        player.velocity.y = 0
+        }
 }
 animate()
 
@@ -82,7 +110,7 @@ addEventListener('keydown', ({ keyCode }) => {
         
             case 87:
             console.log('up')
-            player.velocity.y -= 20
+            player.velocity.y = -10
             break
         
     }
@@ -108,7 +136,7 @@ addEventListener('keyup', ({ keyCode }) => {
         
             case 87:
             console.log('up')
-            player.velocity.y = 10
+            player.velocity.y = -10
             break
         
     }
