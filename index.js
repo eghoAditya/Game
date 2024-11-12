28f8b0a2eee3ea00cc422dfb2dc8193359fb41c1
Aditya <jackuserme8881@gmail.com>
2024-11-12 14:52:13 +0530

    Platform image
    Added Image to the platform

--- index.js
+++ index.js
@@ -1,177 +1,164 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+// Instead of importing the image directly, load it dynamically
+const platformImage = new Image();
+platformImage.src = './imgs/platform.png';  // Path to the image
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+// Log the image object to see it in the console
+console.log(platformImage);
 
-const gravity = 0.5
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
-class Player{
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const gravity = 0.5;
+
+class Player {
     constructor() {
         this.position = {
             x: 100,
             y: 100
-        }
+        };
         this.velocity = {
             x: 0,
             y: 1
-        }
-        this.width = 30
-        this.height = 30
+        };
+        this.width = 30;
+        this.height = 30;
     }
+
     draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 
     update() {
-        this.draw()
-        this.position.y += this.velocity.y
-        this.position.x += this.velocity.x
+        this.draw();
+        this.position.y += this.velocity.y;
+        this.position.x += this.velocity.x;
 
-        if (this.position.y + this.height +
-            this.velocity.y <= canvas.height)
-            this.velocity.y += gravity
-        else this.velocity.y = 0
-        
-        
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+            this.velocity.y += gravity;
+        else this.velocity.y = 0;
     }
 }
 
-class Platform{
-    constructor({x,y}) {
+class Platform {
+    constructor({ x, y }) {
         this.position = {
             x,
             y
-        }
-        this.width = 200
-        this.height = 20        
+        };
+        this.width = 200;
+        this.height = 20;
     }
+
     draw() {
-        c.fillStyle = 'blue'
-        c.fillRect(this.position.x,
-            this.position.y, this.width, this.height)
-        
+        // Draw the image on the platform instead of a fillRect
+        c.drawImage(platformImage, this.position.x, this.position.y, this.width, this.height);
     }
 }
 
-const player = new Player()
-const platforms = [new Platform({ x: 200, y: 100 }),
-new Platform({x:500, y:200})]
+const player = new Player();
+const platforms = [
+    new Platform({ x: 200, y: 100 }),
+    new Platform({ x: 500, y: 200 })
+];
 const keys = {
     right: {
         pressed: false
     },
     left: {
         pressed: false
-    },
-}
+    }
+};
 
-//Track how far player have moved --- © Code by | Aditya Tomar
-
-    let scrollOffset = 0
+let scrollOffset = 0;
 
 function animate() {
-    requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
-    player.update()
-//Multiple platform fucntioning --- © Code by | Aditya Tomar
-    platforms.forEach(platform => {
-        platform.draw()
-    })
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
     
+    // Draw all platforms
+    platforms.forEach(platform => {
+        platform.draw();
+    });
 
     if (keys.right.pressed && player.position.x < 400) {
-        player.velocity.x = 5
-    }
-    else if (keys.left.pressed && player.position.x > 100) {
-        player.velocity.x = -5
+        player.velocity.x = 5;
+    } else if (keys.left.pressed && player.position.x > 100) {
+        player.velocity.x = -5;
     } else {
-        player.velocity.x = 0
+        player.velocity.x = 0;
 
-//Illusion of M=movement of platform --- © Code by | Aditya Tomar
+        // Illusion of movement of the platform
         if (keys.right.pressed) {
-            scrollOffset +=5
+            scrollOffset += 5;
             platforms.forEach(platform => {
-                platform.position.x -= 5
-            })
-           
-        }
-        else if (keys.left.pressed) {
-            scrollOffset -=5
+                platform.position.x -= 5;
+            });
+        } else if (keys.left.pressed) {
+            scrollOffset -= 5;
             platforms.forEach(platform => {
-                platform.position.x += 5
-            })
-            
-
+                platform.position.x += 5;
+            });
         }
     }
+
+    // Platform collision detection
     platforms.forEach(platform => {
-//platform collision detection --- © Code by | Aditya Tomar
-        if (player.position.y + player.height
-            <= platform.position.y &&
-            player.position.y + player.height +
-            player.velocity.y >= platform.position.y &&
+        if (player.position.y + player.height <= platform.position.y &&
+            player.position.y + player.height + player.velocity.y >= platform.position.y &&
             player.position.x + player.width >= platform.position.x &&
             player.position.x <= platform.position.x + platform.width) {
-            player.velocity.y = 0
+            player.velocity.y = 0;
         }
-    })
+    });
 
     if (scrollOffset > 2000) {
-        console.log('You Win')
+        console.log('You Win');
     }
 }
-animate()
+
+animate();
 
 addEventListener('keydown', ({ keyCode }) => {
     switch (keyCode) {
-
-            case 65:
-            console.log('left')
-            keys.left.pressed = true
-            break
-        
-            case 83:
-                console.log('down')
-            break
-        
-            case 68:
-            console.log('right')
-            keys.right.pressed = true
-            break
-        
-            case 87:
-            console.log('up')
-            player.velocity.y = -10
-            break
-        
+        case 65:
+            console.log('left');
+            keys.left.pressed = true;
+            break;
+        case 83:
+            console.log('down');
+            break;
+        case 68:
+            console.log('right');
+            keys.right.pressed = true;
+            break;
+        case 87:
+            console.log('up');
+            player.velocity.y = -10;
+            break;
     }
-
-})
+});
 
 addEventListener('keyup', ({ keyCode }) => {
     switch (keyCode) {
-
-            case 65:
-            console.log('left')
-            keys.left.pressed = false
-            break
-        
-            case 83:
-                console.log('down')
-            break
-        
-            case 68:
-            console.log('right')
-            keys.right.pressed = false
-            break
-        
-            case 87:
-            console.log('up')
-            player.velocity.y = -10
-            break
-        
+        case 65:
+            console.log('left');
+            keys.left.pressed = false;
+            break;
+        case 83:
+            console.log('down');
+            break;
+        case 68:
+            console.log('right');
+            keys.right.pressed = false;
+            break;
+        case 87:
+            console.log('up');
+            player.velocity.y = -10;
+            break;
     }
-    
-})
+});
